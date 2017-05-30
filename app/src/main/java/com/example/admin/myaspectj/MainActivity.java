aee266c62fd73f94810a00f737a0a76a0a2e10ee
pwd-ctf <1959403248@qq.com>
2017-05-30 20:48:06 +0800

    233

--- app/src/main/java/com/example/admin/myaspectj/MainActivity.java
+++ app/src/main/java/com/example/admin/myaspectj/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.admin.myaspectj;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myaspectj.anno.TestAspectJ;
 
@@ -13,30 +19,249 @@ import java.util.stream.Stream;
 public class MainActivity extends AppCompatActivity {
     public Button btn;
     public TextView PizzaType;
-    public ListView listView;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inint();
-        PizzaStoreTest(PizzaType.toString());
-    }
-    @TestAspectJ(eventName = "dogg")
-    public void test(String strings){
-        String dog = strings;
+        verifyStoragePermissions(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PizzaStoreTest(PizzaType.getText().toString());
+            }
+        });
     }
 
-    public static void PizzaStoreTest (String food) {
 
-        PizzaStore pizzaStore = new NYPizzaStore();
-        Pizza pizza = pizzaStore.orderPizza(food);
-        pizza = pizzaStore.orderPizza(food);
+    @TestAspectJ(eventName = "Store")
+    public  void PizzaStoreTest (String food) {
+        Pizza pizza=new Pizza();
+        if(food.equals("sty1")){
+            sty1(pizza);
+        }else if(food.equals("sty2")){
+            sty2(pizza);
+        }else if(food.equals("sty3")){
+            sty3(pizza);
+        }else if(food.equals("sty4")){
+            sty4(pizza);
+        }else if(food.equals("sty5")){
+            sty5(pizza);
+        }else if(food.equals("sty6")){
+            sty6(pizza);
+        }else if(food.equals("sty7")){
+            sty7(pizza);
+        }else if(food.equals("sty8")){
+            sty8(pizza);
+        }else if(food.equals("sty9")){
+            sty9(pizza);
+        }else if(food.equals("sty10")){
+            sty10(pizza);
+        }else if(food.equals("sty11")){
+            sty11(pizza);
+        }else if(food.equals("sty12")){
+            sty12(pizza);
+        }else if(food.equals("sty13")){
+            sty13(pizza);
+        }else if(food.equals("sty14")){
+            sty14(pizza);
+        }else if(food.equals("sty15")){
+            sty15(pizza);
+        }else if(food.equals("sty16")){
+            sty16(pizza);
+        }else if(food.equals("sty17")){
+            sty17(pizza);
+        }else if(food.equals("sty18")){
+            sty18(pizza);
+        }else if(food.equals("sty19")){
+            sty19(pizza);
+        }else if(food.equals("sty20")){
+            sty20(pizza);
+        }
 
     }
+    @TestAspectJ(eventName = "Store")
+    public  void sty1(Pizza pizza){
+        pizza.add1=add1();
+        pizza.add2=add2();
+        pizza.add3=add3();
+    }
+    public  void sty2(Pizza pizza){
+        pizza.add2=add2();
+        pizza.add3=add3();
+        pizza.add4=add4();
+    }
+    public  void sty3(Pizza pizza){
+        pizza.add3=add3();
+        pizza.add4=add4();
+        pizza.add5=add5();
+    }
+    public  void sty4(Pizza pizza){
+        pizza.add4=add4();
+        pizza.add5=add5();
+        pizza.add6=add6();
+    }
+    public  void sty5(Pizza pizza){
+        pizza.add5=add5();
+        pizza.add6=add6();
+        pizza.add7=add7();
+    }
+    public  void sty6(Pizza pizza){
+        pizza.add6=add6();
+        pizza.add7=add7();
+        pizza.add8=add8();
+    }
+    public  void sty7(Pizza pizza){
+        pizza.add7=add7();
+        pizza.add8=add8();
+        pizza.add9=add9();
+    }
+    public  void sty8(Pizza pizza){
+        pizza.add8=add8();
+        pizza.add9=add9();
+        pizza.add10=add10();
+    }
+    public  void sty9(Pizza pizza){
+        pizza.add1=add1();
+        pizza.add9=add9();
+        pizza.add10=add10();
+    }
+    public  void sty10(Pizza pizza){
+        pizza.add1=add1();
+        pizza.add2=add2();
+        pizza.add10=add10();
+    }
+    public  void sty11(Pizza pizza){
+        pizza.add1=add1();
+        pizza.add3=add3();
+        pizza.add5=add5();
+    }
+    public  void sty12(Pizza pizza){
+        pizza.add2=add2();
+        pizza.add4=add4();
+        pizza.add6=add6();
+    }
+    public  void sty13(Pizza pizza){
+        pizza.add3=add3();
+        pizza.add5=add5();
+        pizza.add7=add7();
+    }
+    public  void sty14(Pizza pizza){
+        pizza.add4=add4();
+        pizza.add6=add6();
+        pizza.add8=add8();
+    }
+    public  void sty15(Pizza pizza){
+        pizza.add5=add5();
+        pizza.add7=add7();
+        pizza.add9=add9();
+    }
+    public  void sty16(Pizza pizza){
+        pizza.add6=add6();
+        pizza.add8=add8();
+        pizza.add10=add10();
+    }
+    public  void sty17(Pizza pizza){
+        pizza.add1=add1();
+        pizza.add7=add7();
+        pizza.add9=add9();
+    }
+    public  void sty18(Pizza pizza){
+        pizza.add2=add2();
+        pizza.add8=add8();
+        pizza.add10=add10();
+    }
+    public  void sty19(Pizza pizza){
+        pizza.add1=add1();
+        pizza.add3=add3();
+        pizza.add9=add9();
+    }
+    public  void sty20(Pizza pizza){
+        pizza.add2=add2();
+        pizza.add4=add4();
+        pizza.add10=add10();
+    }
+    @TestAspectJ(eventName = "add1")
+    public  int add1(){
+        //Toast.makeText(MainActivity.this,"add1",Toast.LENGTH_SHORT).show();
+        return 1;
+    }
+    @TestAspectJ(eventName = "add2")
+    public  int add2(){
+        //Toast.makeText(MainActivity.this,"add2",Toast.LENGTH_SHORT).show();
+        return 1;
+    }
+    @TestAspectJ(eventName = "add3")
+    public  int add3(){
+        //Toast.makeText(MainActivity.this,"add3",Toast.LENGTH_SHORT).show();
+        return 1;
+    }
+    @TestAspectJ(eventName = "add4")
+    public  int add4(){
+        //Toast.makeText(MainActivity.this,"add4",Toast.LENGTH_SHORT).show();
+        return 1;
+    }
+    @TestAspectJ(eventName = "add5")
+    public  int add5(){
+        Toast.makeText(MainActivity.this,"add5",Toast.LENGTH_SHORT).show();
+        return 1;
+    }
+    @TestAspectJ(eventName = "add6")
+    public  int add6(){
+        Toast.makeText(MainActivity.this,"add6",Toast.LENGTH_SHORT).show();
+        return 1;
+    }
+    @TestAspectJ(eventName = "add7")
+    public  int add7(){
+        Toast.makeText(MainActivity.this,"add7",Toast.LENGTH_SHORT).show();
+        return 1;
+    }
+    @TestAspectJ(eventName = "add8")
+    public  int add8(){
+        Toast.makeText(MainActivity.this,"add8",Toast.LENGTH_SHORT).show();
+        return 1;
+    }
+    @TestAspectJ(eventName = "add9")
+    public  int add9(){
+        Toast.makeText(MainActivity.this,"add9",Toast.LENGTH_SHORT).show();
+        return 1;
+    }
+    @TestAspectJ(eventName = "add10")
+    public  int add10(){
+        Toast.makeText(MainActivity.this,"add10",Toast.LENGTH_SHORT).show();
+        return 1;
+    }
+
 
     public void inint(){
         btn=(Button) findViewById(R.id.btn_order);
         PizzaType=(TextView)findViewById(R.id.editType);
-        listView=(ListView)findViewById(R.id.listView);
+    }
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
 }
--- myaspectj2/src/main/java/com/example/myaspectj/TestAspectJ.java
+++ myaspectj2/src/main/java/com/example/myaspectj/TestAspectJ.java
@@ -1,8 +1,15 @@
 package com.example.myaspectj;
 
+import android.Manifest;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+
+import com.example.myaspectj.anno.TestAspect;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -10,6 +17,11 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 
 /**
  * Created by Administrator on 2016/9/2.
@@ -19,39 +31,126 @@ public class TestAspectJ {
 
     private static final String METHOD_EXECUTION = "execution(* *..MainActivity+.onCreate(..))";
 
-    private static final String METHOD_CALL = "call(* *..MainActivity+.test(..))";
-
+    private static final String METHOD_CALL = "call(* *..MainActivity+.PizzaStoreTest(..))";
+    private static final String ALL_EXECUTION="execution(* *..MainActivity.*(..))";
     private String TAG = "YourButterfly";
 
     @Before(METHOD_CALL)
-    public void class1(JoinPoint joinPoint){
-        Log.e(TAG,joinPoint.getArgs().clone()[0].toString());
+    public void test(JoinPoint joinPoint){
+        Log.e(TAG,joinPoint.getSignature().toString());
     }
 
-    @Pointcut(METHOD_EXECUTION)
-    public void methodExecution() {
+    @Before(ALL_EXECUTION)
+    public void allExecution(JoinPoint joinPoint){
+        String tmp=joinPoint.getSignature().getName();
+        if(tmp.equals("sty1")){
+            writeFile("1 1 1 0 0 0 0 0 0 0");
+        }
+        else if(tmp.equals("sty2")){
+            writeFile("0 1 1 1 0 0 0 0 0 0");
+        }
+        else if(tmp.equals("sty3")){
+            writeFile("0 0 1 1 1 0 0 0 0 0");
+        }
+        else if(tmp.equals("sty4")){
+            writeFile("0 0 0 1 1 1 0 0 0 0");
+        }
+        else if(tmp.equals("sty5")){
+            writeFile("0 0 0 0 1 1 1 0 0 0");
+        }
+        else if(tmp.equals("sty6")){
+            writeFile("0 0 0 0 0 1 1 1 0 0");
+        }
+        else if(tmp.equals("sty7")){
+            writeFile("0 0 0 0 0 0 1 1 1 0");
+        }
+        else if(tmp.equals("sty8")){
+            writeFile("0 0 0 0 0 0 0 1 1 1");
+        }
+        else if(tmp.equals("sty9")){
+            writeFile("1 0 0 0 0 0 0 0 1 1");
+        }
+        else if(tmp.equals("sty10")){
+            writeFile("1 1 0 0 0 0 0 0 0 1");
+        }
+        else if(tmp.equals("sty11")){
+            writeFile("1 0 1 0 1 0 0 0 0 0");
+        }
+        else if(tmp.equals("sty12")){
+            writeFile("0 1 0 1 0 1 0 0 0 0");
+        }
+        else if(tmp.equals("sty13")){
+            writeFile("0 0 1 0 1 0 1 0 0 0");
+        }
+        else if(tmp.equals("sty14")){
+            writeFile("0 0 0 1 0 1 0 1 0 0");
+        }
+        else if(tmp.equals("sty15")){
+            writeFile("0 0 0 0 1 0 1 0 1 0");
+        }
+        else if(tmp.equals("sty16")){
+            writeFile("0 0 0 0 0 1 0 1 0 1");
+        }
+        else if(tmp.equals("sty17")){
+            writeFile("1 0 0 0 0 0 1 0 1 0");
+        }
+        else if(tmp.equals("sty18")){
+            writeFile("0 1 0 0 0 0 0 1 0 1");
+        }
+        else if(tmp.equals("sty19")){
+            writeFile("1 0 1 0 0 0 0 0 1 0");
+        }
+        else if(tmp.equals("sty20")){
+            writeFile("0 1 0 1 0 0 0 0 0 1");
+        }
 
+
+        Log.e(TAG,joinPoint.getSignature().getName());
     }
 
-    @Pointcut(METHOD_CALL)
-    public void methodCall() {
+    public static boolean isSdCardExist() {
+        return Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED);
+    }
+    public static String getSdCardPath() {
+        boolean exist = isSdCardExist();
+        String sdpath = "";
+        if (exist) {
+            sdpath = Environment.getExternalStorageDirectory()
+                    .getAbsolutePath();
+        } else {
+            sdpath = "不适用";
+        }
+        return sdpath;
 
     }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    @Around("methodExecution()")
-    public void aroundMethodExecution(ProceedingJoinPoint joinPoint) throws Throwable {
-        joinPoint.proceed();
-        String result = "-----------------------------MethodExecution";
-        Log.e(TAG, result);
+    public static String getDefaultFilePath() {
+        String filepath = "";
+        File file = new File(Environment.getExternalStorageDirectory(),
+                "test.txt");
+        if (file.exists()) {
+            filepath = file.getAbsolutePath();
+        } else {
+            filepath = "不适用";
+        }
+        return filepath;
     }
+    public static  void writeFile(String content){
+        try {
+            Log.e("test",getSdCardPath());
+            Log.e("test",getDefaultFilePath());
+            File file = new File(Environment.getExternalStorageDirectory(),
+                    "test.txt");
+            //第二个参数意义是说是否以append方式添加内容
+            FileWriter writer = new FileWriter(file.getAbsoluteFile(), true);
+            writer.write(content);
+            writer.write("\r\n");
+            writer.close();
+            System.out.println("写入成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    @Around("methodCall()")
-    public Object arouneMethodCall(ProceedingJoinPoint joinPoint){
-        String name = "qqq";
-        Log.e(TAG,name);
-        return name;
     }
 
 }
